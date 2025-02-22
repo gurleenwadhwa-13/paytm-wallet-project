3d125fd8d44b9744fab7f6af307acb7dd7c3564b
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-02-22 08:17:39 -0400

    initiated FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { RecoilRoot } from "recoil";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <RecoilRoot>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoneyModal />} /> */}
+      </Routes>
+    </BrowserRouter>
+    </RecoilRoot>
   )
 }
 
